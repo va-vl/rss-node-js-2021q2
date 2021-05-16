cb05772637a4e037d56a4bb5be4f450c73cd0cf7
va-z <vasilizadorojnyuk@gmail.com>
2021-05-17 00:44:41 +0500

    fix: fix error object imports

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,6 +1,6 @@
 const BoardNotFoundError = require('./not-found/board-not-found-error');
-const TaskNotFoundError = require('./not-found/board-not-found-error');
-const UserNotFoundError = require('./not-found/board-not-found-error');
+const TaskNotFoundError = require('./not-found/task-not-found-error');
+const UserNotFoundError = require('./not-found/user-not-found-error');
 const DataCorruptedError = require('./data-corrupted-error');
 
 module.exports = {
