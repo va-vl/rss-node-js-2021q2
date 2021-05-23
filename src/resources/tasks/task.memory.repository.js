50150e76ef19106deb57c2ce9976ed01f461caad
va-z <vasilizadorojnyuk@gmail.com>
2021-05-24 03:33:58 +0500

    docs: update tasks jsdoc

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -3,22 +3,25 @@
  * @module task/repository
  */
 const DB = require('../../db/db.memory');
-const Task = require('./task.model');
 const { TaskNotFoundError } = require('../../errors');
 
 const TABLE_NAME = 'Tasks';
 
 /**
- * @param {String} boardId
- * @returns {<Task>}
+ * Retrieves all Tasks from a given boardId
+ * @param {String} boardId - id of a board Task belongs to
+ * @returns {Promise<Array<Task>>} - a promise resolving to array of Tasks
  */
 const getAll = async (boardId) =>
   DB.getEntitiesByProps(TABLE_NAME, { boardId });
 
 /**
- * @param {String} boardId
- * @param {String} id
- * @returns {Task}
+ * Retrieves a Task instance by id and boardId
+ * @param {String} boardId - id of a board Task belongs to
+ * @param {String} id - task id
+ * @throws {DataCorruptedError} - rejects if more than one task with id was found on the board
+ * @throws {TaskNotFoundError} - rejects if no Tasks with id was found on the board
+ * @returns {Promise<Task>} - a promise resolving to Task instance
  */
 const getById = async (boardId, id) => {
   const task = await DB.getEntityByIdAndProps(TABLE_NAME, id, { boardId });
@@ -31,16 +34,22 @@ const getById = async (boardId, id) => {
 };
 
 /**
- * @param {Object} task
- * @returns {Task}
+ * Forwards a task instance to database
+ * @param {Task} taskInstance - a Task instance
+ * @throws {DataCorruptedError} - rejects if more task with id already exists on the board
+ * @returns {Promise<Task>} - a promise resolving to Task instance
  */
-const create = async (task) => DB.createEntity(TABLE_NAME, task);
+const create = async (taskInstance) =>
+  DB.createEntity(TABLE_NAME, taskInstance);
 
 /**
- * @param {String} boardId
- * @param {String} id
- * @param {Object} props
- * @returns {Task}
+ * Forwards set of new props to be applied to task on board
+ * @param {String} boardId - id of a board where task is found
+ * @param {String} id - task id
+ * @param {Object} props - a collection of key: value pairs
+ * @throws {DataCorruptedError} - rejects if more than one task with id was found on the board
+ * @throws {TaskNotFoundError} - rejects if no Tasks with id was found on the board
+ * @returns {Promise<Task>} - a promise resolving to updated Task
  */
 const update = async (boardId, id, props) => {
   const updatedTask = await DB.updateEntity(TABLE_NAME, id, props);
@@ -53,8 +62,12 @@ const update = async (boardId, id, props) => {
 };
 
 /**
- * @param {String} boardId
- * @param {String} id
+ * Deletes task from a given board
+ * @param {String} boardId - id of a board
+ * @param {String} id - task id
+ * @throws {DataCorruptedError} - rejects if more than one task with id was found on the board
+ * @throws {TaskNotFoundError} - rejects if no Tasks with id was found on the board
+ * @returns {void}
  */
 const remove = async (boardId, id) => {
   const removedTask = await DB.deleteEntity(TABLE_NAME, id);
@@ -65,7 +78,9 @@ const remove = async (boardId, id) => {
 };
 
 /**
- * @param {String} boardId
+ * Deletes all tasks on a given board
+ * @param {String} boardId - board id
+ * @returns {void}
  */
 const removeAllOnBoard = async (boardId) => {
   const removedTasks = await DB.getEntitiesByProps(TABLE_NAME, { boardId });
@@ -76,14 +91,16 @@ const removeAllOnBoard = async (boardId) => {
 };
 
 /**
- * @param {String} userId
+ * If userId of a task equals the provided userId, sets Task's userId to null
+ * @param {String} userId - user id
+ * @returns {void}
  */
 const removeUserBinding = async (userId) => {
   const tasks = await DB.getAllEntities(TABLE_NAME);
 
   tasks.forEach(async (task) => {
     if (task.userId === userId) {
-      await update(task.boardId, task.id, new Task({ ...task, userId: null }));
+      await DB.updateEntity(TABLE_NAME, task.id, { userId: null });
     }
   });
 };
--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -6,49 +6,67 @@ const Task = require('./task.model');
 const tasksRepo = require('./task.memory.repository');
 
 /**
- * @param {String} boardId
- * @returns {<Task>}
+ * Forwards boardId to task/repository and retrieves all tasks on that board
+ * @param {String} boardId - board id
+ * @returns {Promise<Array<Task>>} - promise resolving to array of Tasks on Board
  */
-const getAll = (boardId) => tasksRepo.getAll(boardId);
+const getAll = async (boardId) => tasksRepo.getAll(boardId);
 
 /**
- * @param {String} boardId
- * @param {String} id
+ * Forwards boardId and task id to task/repository and retrieves task with id on board
+ * @param {String} boardId - board id
+ * @param {String} id - task id
+ * @throws {DataCorruptedError} - rejects if more than one task with id was found on the board
+ * @throws {TaskNotFoundError} - rejects if no Tasks with id was found on the board
+ * @returns {Promise<Task>} - promise resolving to Task on Board
+ */
+const getById = async (boardId, id) => tasksRepo.getById(boardId, id);
+
+/**
+ * Creates a new Task instance from props and forwards to task/repository to be added to db
+ * @param {String} boardId - board id
+ * @param {Object} props - a collection of key: value pairs
+ * @throws {DataCorruptedError} - rejects if more task with id already exists on the board
+ * @returns {Promise<Task>} - promise resolving to Task on Board
+ */
+const create = async (boardId, props) =>
+  tasksRepo.create(new Task({ ...props, boardId }));
+
+/**
+ * Forwards new props to be applied to task on board
+ * @param {String} boardId - board id
+ * @param {String} id - task id
+ * @param {Object} props - a collection of key: value pairs
+ * @throws {DataCorruptedError} - rejects if more than one task with id was found on the board
+ * @throws {TaskNotFoundError} - rejects if no Tasks with id was found on the board
  * @returns {Task}
  */
-const getById = (boardId, id) => tasksRepo.getById(boardId, id);
+const update = async (boardId, id, props) =>
+  tasksRepo.update(boardId, id, props);
 
 /**
- * @param {String} boardId
- * @param {Object} task
- * @returns {Task}
- */
-const create = (boardId, task) =>
-  tasksRepo.create(new Task({ ...task, boardId }));
-
-/**
+ * Calls task/repository to remove task on board
  * @param {String} boardId
  * @param {String} id
- * @param {Object} task
- * @returns {Task}
+ * @throws {DataCorruptedError} - rejects if more than one task with id was found on the board
+ * @throws {TaskNotFoundError} - rejects if no Tasks with id was found on the board
+ * @returns {void}
  */
-const update = (boardId, id, task) => tasksRepo.update(boardId, id, task);
+const remove = async (boardId, id) => tasksRepo.remove(boardId, id);
 
 /**
- * @param {String} boardId
- * @param {String} id
+ * Forwards boardId to task/repository to find and remove all tasks on board
+ * @param {String} boardId - board id
+ * @returns {void}
  */
-const remove = (boardId, id) => tasksRepo.remove(boardId, id);
+const removeAllOnBoard = async (boardId) => tasksRepo.removeAllOnBoard(boardId);
 
 /**
- * @param {String} boardId
+ * Forwards userId to task/repository to unbind user from all tasks
+ * @param {String} userId - user id
+ * @return {void}
  */
-const removeAllOnBoard = (boardId) => tasksRepo.removeAllOnBoard(boardId);
-
-/**
- * @param {String} userId
- */
-const removeUserBinding = (userId) => tasksRepo.removeUserBinding(userId);
+const removeUserBinding = async (userId) => tasksRepo.removeUserBinding(userId);
 
 module.exports = {
   getAll,
