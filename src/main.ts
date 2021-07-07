13a85c0d53ce405dfc3311ad6011f0c23a3f5aa8
Vasili Zadorojnyuk <vasilizadorojnyuk@gmail.com>
2021-07-07 17:09:29 +0500

    feat: add yaml docs from express-app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as path from 'path';
+import * as yaml from 'yamljs';
 //
 import { AppModule } from './app.module';
 import { seedAdmin } from './utils/seed-admin';
@@ -10,6 +13,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   await seedAdmin();
+
+  const swaggerDocument = yaml.load(path.resolve('doc/api.yaml'));
+  SwaggerModule.setup('doc', app, swaggerDocument);
 
   const port = app.get(ConfigService).get('PORT');
 
